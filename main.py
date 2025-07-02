dc8866bff808afd905b55fb540491a28c6d2e900
Pranaw Mishra <pranawmishra73@gmail.com>
2025-07-02 13:38:37 +0530

    v1.1.0

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from tests.test_local import test_main
+from tests.test_configuration import test_configuration_complete
 import asyncio
 
 async def main():
     print("Hello from sherlock-ai!")
-    await test_main()
+    # await test_main()
+    await test_configuration_complete()
 
 
 if __name__ == "__main__":
